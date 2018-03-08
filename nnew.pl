c3ff7602f572901bd1fbd7b84c8b1a4cb218c433
SAnjiHolic <sanhah0322@naver.com>
2018-03-08 17:31:11 +0900

    20180308

--- nnew.pl
+++ nnew.pl
@@ -12,6 +12,40 @@ foreach(@res){
 			if($' =~/^#"/){$link=undef;}
 			else{ 
 				$link=$';
+
+				if($link =~ /nclicks\(rig\.rankpol\)/){
+					$link .=" junci  ";
+					$link =~ s/nclicks\(rig\.rankpol\)//g;
+				}
+				elsif($link =~ /nclicks\(rig\.rankeco\)/){
+					$link .=" gungjae ";
+					$link =~ s/nclicks\(rig\.rankeco\)//g;
+				}
+				elsif($link =~ /nclicks\(rig\.ranksoc\)/){
+					$link .=" sahoi ";
+					$link =~ s/nclicks\(rig\.ranksoc\)//g;
+				}
+				elsif($link =~ /nclicks\(rig\.ranklif\)/){
+					$link .=" moonhwa ";
+					$link =~ s/nclicks\(rig\.ranklif\)//g;
+			}
+				elsif($link =~ /nclicks\(rig\.rankwor\)/){
+					$link .=" jonghap ";
+					$link =~ s/nclicks\(rig\.rankwor\)//g;
+				}
+				elsif($link =~ /nclicks\(rig\.ranksci\)/){
+					$link .=" it/sic ";
+					$link =~ s/nclicks\(rig\.ranksci\)//g;
+				}
+				elsif ($link =~ /nclicks\(rig\.rankent\)/) {
+					$link .=" TV ";
+					$link =~ s/nclicks\(rig\.rankent\)//g; 
+				}
+				elsif ($link =~ /nclicks\(rig\.rankspo\)/) {
+					$link .=" spor ";
+					$link =~ s/nclicks\(rig\.rankspo\)//g;
+				}
+
 				$link =~ s/ class="*"|"/ /g; 				
 				}
 		}
