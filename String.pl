aac444342cd4f7fec8800d42630606bab6f457d0
SAnjiHolic <sanhah0322@naver.com>
2018-03-21 16:34:28 +0900

    20180321

---
+++ String.pl
@@ -0,0 +1,22 @@
+die "Insert File name\n" if $#ARGV == -1;
+my $chkren = @ARGV[1];
+open(IN, "<", $ARGV[0]) or die "$!\n";	 
+binmode(IN);
+while ($buf16Len = read(IN, $buf16, 51020402)) {
+	@buf16Array = unpack "C*", $buf16;
+	$chkren = 3	if ($chkren == 0);
+	for (my $i = 0; $i < $buf16Len; $i++) {
+		if ($buf16Array[$i] > 32 & $buf16Array[$i] < 126) {
+			$tmp_ren .= chr($buf16Array[$i]); $flag++;
+			}
+		else {
+			if($flag >= $chkren){
+				$tmp_ren .= "\n"; push @ren, $tmp_ren; $tmp_ren = ""; }
+			else{
+				$tmp_ren ="";}
+			$flag = 0;
+			}
+		}
+	}
+close IN;
+foreach(@ren){print;}
