912b14f23a08b6755739132728fa82c0fe242a4b
SAnjiholic <sanhah0322@naver.com>
2018-03-23 20:03:17 +0900

    20180323

--- hex.pl
+++ hex.pl
@@ -1,38 +1,58 @@
 #!/usr/bin/perl
 
-die "파일명을 입력하세요\n" if $#ARGV == -1;
-my $offset = 0; # 번지 변수
+die "Input File name \n" if $#ARGV == -1;
+my ($offset,$address,$line);
 my ($buf16, $buf16Len, @buf16Array, $output, @Leng);
+my $cls = `clear`;
+	while(1){
 
-open(IN, "<", $ARGV[0]) or die "$!\n";
-binmode(IN);
+		$line = 0;
+		@Leng = ();
+		$SIG{'INT'} = \&interrupt;
+		print $cls;
+		open(IN, "<", $ARGV[0]) or die "$!\n";
+		binmode(IN);
 
+		while ($buf16Len = read(IN, $buf16, 16)) {
+			$output = sprintf("%08X:  ", $offset);
+			@buf16Array = unpack "C*", $buf16;
 
-while ($buf16Len = read(IN, $buf16, 16)) {
-	$output = sprintf("%08X:  ", $offset);
-		@buf16Array = unpack "C*", $buf16;
-
-		for (my $i = 0; $i < $buf16Len; $i++) {
-			$output .= " " if ($i == 8);
+			for (my $i = 0; $i < $buf16Len; $i++) {
+				$output .= " " if ($i == 8);
 				$output .= sprintf("%02X ", $buf16Array[$i]);
-		}
-	for (my $i = 0; $i <= (16 - $buf16Len) * 3; $i++) { $output .= " "; }
-	$output .= " " if ($buf16Len < 9);
+			}
+			for (my $i = 0; $i <= (16 - $buf16Len) * 3; $i++) { $output .= " "; }
+			$output .= " " if ($buf16Len < 9);
 
-		for (my $i = 0; $i < $buf16Len; $i++) {
-			if ($buf16Array[$i] >= 0x20 && $buf16Array[$i] <= 0x7E) {
-				$output .= sprintf("%c", $buf16Array[$i]);
-			
-			} else { $output .= "."; }
-		}
+			for (my $i = 0; $i < $buf16Len; $i++) {
+				if ($buf16Array[$i] >= 0x20 && $buf16Array[$i] <= 0x7E) {
+					$output .= sprintf("%c", $buf16Array[$i]);
 
-	$offset += 16;
-	push (@Leng,$output);
+				} else { $output .= "."; }
+			}
+
+			$offset += 16;
+			if($line > 65){last;}
+			if ($address < $offset){
+				push (@Leng,$output);
+				$line++;
+			}	
+		}
+		printf("%08X:  ", $offset) if ($offset == 0);
+		close IN;
+		foreach(@Leng){
+			print;
+			print "\n";
+		}
+		sleep 1;
+		print $cls
+			$offset = 0;
+	}
+
+sub interrupt{
+	my $num;
+	print "Goto Address : ";
+	chomp($num = <STDIN>); 
+	$address = hex($num);
 }
-printf("%08X:  ", $offset) if ($offset == 0);
-close IN;
-foreach(@Leng){
-	print;
-	print "\n";
-};
 
