1803b7903d7d42df749ea8b7ccc14c702c4f10be
SAnjiholic <sanhah0322@naver.com>
2018-04-17 22:19:47 +0900

    20180417

--- String.pl
+++ String.pl
@@ -1,8 +1,9 @@
 die "Insert File name\n" if $#ARGV == -1;
 my $chkren = @ARGV[1];
+my $size = -s $ARGV[0];
 open(IN, "<", $ARGV[0]) or die "$!\n";	 
 binmode(IN);
-while ($buf16Len = read(IN, $buf16, 51020402)) {
+while ($buf16Len = read(IN, $buf16, $size)) {
 	@buf16Array = unpack "C*", $buf16;
 	$chkren = 3	if ($chkren == 0);
 	for (my $i = 0; $i < $buf16Len; $i++) {
